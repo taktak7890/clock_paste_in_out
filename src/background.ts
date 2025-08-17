35a12b7d8bc3eef429d4a35aba9138594627b5ca
ym <>
2025-08-17 19:51:11 +0900

    設定画面をオプション画面からダイアログに変更

--- src/background.ts
+++ src/background.ts
@@ -1,10 +1,12 @@
 chrome.runtime.onInstalled.addListener(() => {
     chrome.contextMenus.create({
+        documentUrlPatterns: ["https://chat.google.com/*"],
         id: "paste_start",
         title: "出社",
         contexts: ["editable"]
     });
     chrome.contextMenus.create({
+        documentUrlPatterns: ["https://chat.google.com/*"],
         id: "paste_finish",
         title: "退社",
         contexts: ["editable"]
@@ -14,17 +16,17 @@ chrome.runtime.onInstalled.addListener(() => {
 chrome.contextMenus.onClicked.addListener((info: chrome.contextMenus.OnClickData, tab?: chrome.tabs.Tab) => {
     chrome.storage.sync.get(["name"], (data: { name?: string; }) => {
         const name = data.name ?? "";
+        let pasteValue = "";
         if (!name) {
-            console.error("名前が設定されていません。");
-            return;
+            pasteValue = "名前が設定されていません。";
+        } else {
+            const today = new Date();
+            const dateStr = (today.getMonth() + 1) + "/" + today.getDate();
+            const fixedText = (info.menuItemId === "paste_finish") ? "退社" : "出社";
+            // MM/DD 名前 固定文字列
+            //   例) 10/1 山田 出社
+            pasteValue = `${dateStr} ${name} ${fixedText}`;
         }
-        const today = new Date();
-        const dateStr = (today.getMonth() + 1) + "/" + today.getDate();
-        const fixedText = (info.menuItemId === "paste_finish") ? "退社" : "出社";
-
-        // MM/DD 名前 固定文字列
-        //   例) 10/1 山田 出社
-        const pasteValue = `${dateStr} ${name} ${fixedText}`;
 
         chrome.scripting.executeScript({
             target: { tabId: tab.id as number, allFrames: true },
--- src/options.ts
+++ src/options.ts
@@ -3,7 +3,9 @@ document.addEventListener('DOMContentLoaded', () => {
         (document.getElementById("name") as HTMLInputElement).value = data.name ?? "";
     });
 
-    document.getElementById("save")?.addEventListener("click", () => {
+    document.getElementById("optionsForm")?.addEventListener("submit", (e) => {
+        e.preventDefault();
+        // 保存ボタンがクリックされたときの処理
         const name = (document.getElementById("name") as HTMLInputElement).value;
         chrome.storage.sync.set({ name }, () => {
             alert("保存しました");
